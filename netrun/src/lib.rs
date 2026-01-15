9f2945e64b13738a9c9fcd0d954dad9e7900b4e3
Vladas Zakrevskis <146100@gmail.com>
2026-01-15 18:41:26 +0200

    local network scanner

--- netrun/src/lib.rs
+++ netrun/src/lib.rs
@@ -2,9 +2,13 @@
 mod connection;
 pub mod rest;
 #[cfg(not_wasm)]
+mod scan;
+#[cfg(not_wasm)]
 pub mod secret;
 mod tests;
 
 #[cfg(not_wasm)]
 pub use connection::*;
 pub use local_ip_address::*;
+#[cfg(not_wasm)]
+pub use scan::*;
---
+++ netrun/src/scan.rs
@@ -0,0 +1,96 @@
+use std::{
+    iter::once,
+    net::{IpAddr, Ipv4Addr},
+    sync::{
+        Arc,
+        atomic::{AtomicUsize, Ordering},
+    },
+};
+
+use anyhow::Result;
+use log::error;
+use rust_network_scanner::{NetworkScanner, PortScanResult};
+use tokio::{
+    spawn,
+    sync::{Mutex, mpsc::channel},
+    time::{Duration, timeout},
+};
+
+fn local_network() -> Vec<IpAddr> {
+    once(Ipv4Addr::LOCALHOST.into())
+        .chain((1..255).map(|i| Ipv4Addr::new(192, 168, 0, i).into()))
+        .collect()
+}
+
+async fn check_port(ip: IpAddr, port: u16, scanner: &NetworkScanner) -> Result<Vec<PortScanResult>> {
+    let timeout_duration = Duration::from_millis(200);
+
+    let Ok(scan) = timeout(timeout_duration, scanner.scan_ports(ip, port, port)).await else {
+        return Ok(vec![]);
+    };
+
+    Ok(scan?.open_ports)
+}
+
+pub async fn scan_for_port(port: u16) -> Result<Vec<(IpAddr, Vec<PortScanResult>)>> {
+    let local = local_network();
+
+    let result = Arc::new(Mutex::new(vec![]));
+    let finished_counter = Arc::new(AtomicUsize::new(local.len() - 1));
+    let (s, mut r) = channel::<()>(1);
+
+    let s = Arc::new(s);
+
+    for ip in local {
+        let counter = finished_counter.clone();
+        let ss = s.clone();
+        let res = result.clone();
+        spawn(async move {
+            let scanner = NetworkScanner::new();
+
+            let open_ports = check_port(ip, port, &scanner)
+                .await
+                .inspect_err(|err| error!("Failed to check port: {err}"))
+                .unwrap();
+
+            if !open_ports.is_empty() {
+                res.lock().await.push((ip, open_ports));
+            }
+
+            if counter.fetch_sub(1, Ordering::Relaxed) == 0 {
+                ss.send(()).await.unwrap();
+            }
+        });
+    }
+
+    r.recv().await;
+
+    let result = Arc::try_unwrap(result).expect("Failed to extract port results from Arc");
+
+    Ok(result.into_inner())
+}
+
+#[cfg(test)]
+mod test {
+    use pretty_assertions::assert_eq;
+    use rust_network_scanner::PortStatus;
+    use test_log::test;
+
+    use super::*;
+    use crate::Server;
+
+    #[test(tokio::test)]
+    async fn test_scan() -> Result<()> {
+        let _server: Server<(), ()> = Server::new(57779).await?;
+
+        let (ip, open_ports) = scan_for_port(57779).await?.into_iter().next().unwrap();
+
+        let open_port = open_ports.first().unwrap();
+
+        assert_eq!(IpAddr::V4(Ipv4Addr::LOCALHOST), ip);
+        assert_eq!(open_port.port, 57779);
+        assert_eq!(open_port.status, PortStatus::Open);
+
+        Ok(())
+    }
+}
