03028ae91ef1915d976df49d29a001496feb506a
Vladas Zakrevskis <146100@gmail.com>
2026-03-01 16:13:06 +0200

    function is empty indicator

--- netrun/src/function.rs
+++ netrun/src/function.rs
@@ -1,4 +1,11 @@
-use std::{any::type_name, fmt::Debug, sync::Arc};
+use std::{
+    any::type_name,
+    fmt::Debug,
+    sync::{
+        Arc,
+        atomic::{AtomicBool, Ordering},
+    },
+};
 
 use log::error;
 use parking_lot::Mutex;
@@ -6,13 +13,15 @@ use parking_lot::Mutex;
 type BoxedFn<In, Out> = Box<dyn FnMut(In) -> Out + Send>;
 
 pub struct Function<In, Out> {
-    fun: Arc<Mutex<BoxedFn<In, Out>>>,
+    is_empty: AtomicBool,
+    fun:      Arc<Mutex<BoxedFn<In, Out>>>,
 }
 
 impl<In, Out> Default for Function<In, Out> {
     fn default() -> Self {
         Self {
-            fun: Arc::new(Mutex::new(Box::new(|_| {
+            is_empty: AtomicBool::new(true),
+            fun:      Arc::new(Mutex::new(Box::new(|_| {
                 error!("Calling empty function of type: {}", type_name::<Self>());
                 panic!("Calling empty function of type: {}", type_name::<Self>())
             }))),
@@ -23,23 +32,30 @@ impl<In, Out> Default for Function<In, Out> {
 impl<In, Out> Function<In, Out> {
     pub fn new(fun: impl FnMut(In) -> Out + Send + 'static) -> Self {
         Self {
-            fun: Arc::new(Mutex::new(Box::new(fun))),
+            is_empty: AtomicBool::new(false),
+            fun:      Arc::new(Mutex::new(Box::new(fun))),
         }
     }
 
     pub fn replace(&self, fun: impl FnMut(In) -> Out + Send + 'static) {
         *self.fun.lock() = Box::new(fun);
+        self.is_empty.store(false, Ordering::Release);
     }
 
     pub fn call(&self, input: In) -> Out {
         (*self.fun.lock())(input)
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.is_empty.load(Ordering::Acquire)
     }
 }
 
 impl<In, Out> Clone for Function<In, Out> {
     fn clone(&self) -> Self {
         Self {
-            fun: self.fun.clone(),
+            is_empty: AtomicBool::new(self.is_empty()),
+            fun:      self.fun.clone(),
         }
     }
 }
@@ -60,9 +76,15 @@ mod test {
     fn test_function() {
         let fun = Function::<String, HashSet<i32>>::default();
 
+        assert!(fun.is_empty());
+
         assert_eq!(
             "\"Function<alloc::string::String, std::collections::hash::set::HashSet<i32>>\"",
             format!("{fun:?}")
         );
+
+        fun.replace(|_| HashSet::new());
+
+        assert!(!fun.is_empty());
     }
 }
