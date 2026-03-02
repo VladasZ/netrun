a50cd9063cc2251100ba28cd7cf8e87d02336e2b
Vladas Zakrevskis <146100@gmail.com>
2026-03-02 15:28:01 +0200

    simple download function

--- netrun/src/rest/simple.rs
+++ netrun/src/rest/simple.rs
@@ -1,10 +1,35 @@
 use std::collections::BTreeMap;
 
 use anyhow::Result;
+use reqwest::Client;
 use serde::de::DeserializeOwned;
 
 use crate::rest::request::request_object;
 
 pub async fn get<T: DeserializeOwned>(url: impl ToString) -> Result<T> {
     request_object(super::Method::Get, url, BTreeMap::default(), None).await
+}
+
+pub async fn download(url: impl ToString) -> Result<Vec<u8>> {
+    let url = url.to_string();
+    let client = Client::new();
+    let bytes = client.get(&url).send().await?.bytes().await?;
+    Ok(bytes.to_vec())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[cfg(not_wasm)]
+    mod not_wasm_tests {
+        use super::*;
+
+        #[tokio::test]
+        async fn test_download() -> Result<()> {
+            let bytes = download("https://www.lrt.lt/img/2026/02/26/2327389-490277-615x345.jpg").await?;
+            assert_eq!(bytes.len(), 40246);
+            Ok(())
+        }
+    }
 }
